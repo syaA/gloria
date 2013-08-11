c26278613b6bdd0155163556202d5bfb97450b8e
syaA <sto.shei@gmail.com>
2013-08-11 14:34:21 +0900

    sdl-image を現状の DLL に合わせてロードするように。

--- main.lisp
+++ main.lisp
@@ -1,6 +1,17 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload :lispbuilder-sdl))
+  (ql:quickload :lispbuilder-sdl)
+  (ql:quickload :cl-opengl)
+  (defun sdl-image-alternative-library (basename)
+    (cond
+      ((string= basename "libpng12-0.dll") "libpng15-15.dll")
+      ((string= basename "jpeg.dll") "libjpeg-8.dll")
+      ((string= basename "libtiff-3.dll") "libtiff-5.dll")))
+  (handler-bind ((simple-error ;cffi:load-foreign-library-error
+		  #'(lambda (c)
+		      (use-value (sdl-image-alternative-library
+				  (caar (simple-condition-format-arguments c)))))))
+    (ql:quickload :lispbuilder-sdl-image)))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (load "load.lisp")
