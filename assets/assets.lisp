20d7fffadda5dae35dca966e861e36222d962dfd
syaA <sto.shei@gmail.com>
2013-08-13 17:00:07 +0900

    武器スプライト追加
     これは自作

--- assets/assets.lisp
+++ assets/assets.lisp
@@ -6,5 +6,7 @@
  (sprite-pattern-animation char1-e (char1-7 char1-8 char1-7 char1-6) 10 t)
  (sprite-pattern-animation char1-n (char1-10 char1-11 char1-10 char1-9) 10 t)
  (sprite-animation-set char1-set char1-n char1-s char1-e char1-w)
+ (texture weapon-sheet "weapon.tga")
+ (sprite sword weapon-sheet 32 32 1 1)
  )
 
